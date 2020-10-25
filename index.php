b331cf3354200d63cf318ff592360db19f7ba867
Daniel Osorio <daniel199531@outlook.es>
2020-10-25 16:40:03 -0500

    se agraga texto e imagenes

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>
+        
+    </title>
+    <link rel="stylesheet" href="estilos.css" type="text/css">
+</head>
+<body>
+    <h1>welcome to the world of programing</h1> <br>
+
+    <div id="imagen1">
+        <img src="imagenes/imagen1.jpg" alt="Programacion" width="500" height="300"> 
+        <p>La programación es el proceso utilizado para idear y ordenar las acciones necesarias para realizar <br>
+            un proyecto, preparar ciertas máquinas o aparatos para que empiecen a funcionar en el momento y en  <br>
+            la forma deseados o elaborar programas para su empleo en computadoras.</p>
+    </div>
+
+    <div id="imagen2">
+        <img src="imagenes/imagen2.jpg" alt="programacion" width="500" height="300">
+        <p>Un lenguaje de programación es un lenguaje formal (o artificial, es decir, un  <br>
+            lenguaje con reglas gramaticales bien definidas) que le proporciona a una persona, <br>
+            en este caso el programador, la capacidad de escribir (o programar) <br>
+            una serie de instrucciones o secuencias de órdenes en forma de algoritmos con el fin de controlar <br>
+            el comportamiento físico o lógico de una computadora, de manera que se puedan obtener diversas  <br>
+            clases de datos o ejecutar determinadas tareas.<br>
+            A todo este conjunto de órdenes escritas mediante un lenguaje de programación se le denomina programa</p>
+    </div>
+    
+    <div>
+        <form method="post" action="pagina2.php">
+            Ingrese su nombre:
+            <input type="text" name="nombre">
+            <br>
+            ¿Le gusto el contenido ?
+            <br>
+            <input type="radio" name="radio1" value="si">si
+            <br>
+           <input type="radio" name="radio1" value="no">no
+           <br>
+           <input type="submit" value="confirmar">
+           <br>
+        </form>        
+    </div>
+
+</body>
+</html>
